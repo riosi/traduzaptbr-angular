32c860284f0e12e77caaac47182f476316dd3c35
Ingryd Rios <riosingryd@gmail.com>
2021-01-09 21:27:17 -0300

    Fixed app name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('app1 app is running!');
+    expect(await page.getTitleText()).toEqual('traduzaptbr-angular app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/app1'),
+      dir: require('path').join(__dirname, './coverage/traduzaptbr-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
